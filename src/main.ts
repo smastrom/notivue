0d8b8958b2480ef9004a549cd5b15ce7c3ade1e9
“adrenochrom3” <71175686+adrenochrom3@users.noreply.github.com>
2023-02-05 21:19:37 +0100

    defined api multiple instances, add install plugin fn

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import { notify } from './notify';
 
-const app = createApp(App).mount('#app');
+createApp(App).use(notify).mount('#app');
