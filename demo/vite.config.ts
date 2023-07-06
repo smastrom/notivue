c6bafd0dcfacf5ff6f0b7bdfcf2153f5ccb3110d
smastrom <60471784+smastrom@users.noreply.github.com>
2023-07-07 00:36:52 +0200

    up deps, release v1.0.4

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
-import postCSSNesting from 'postcss-nesting'
 
 export default defineConfig({
    css: {
-      postcss: {
-         plugins: [postCSSNesting],
+      transformer: 'lightningcss',
+      lightningcss: {
+         drafts: {
+            nesting: true,
+         },
       },
+   },
+   build: {
+      cssMinify: 'lightningcss',
    },
    resolve: {
       alias: {
