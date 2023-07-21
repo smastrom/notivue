fc964ac10392fd030afb80ddbe1d0beb5f0da11e
smastrom <60471784+smastrom@users.noreply.github.com>
2023-07-21 14:27:33 +0200

    add useFocusEvents

---
+++ packages/notivue/Notivue/composables/useFocusEvents.ts
@@ -0,0 +1,49 @@
+import { watch } from 'vue'
+import { useElements, useItems, usePointer } from '@/core/useStore'
+
+export function useFocusEvents() {
+   const items = useItems()
+   const elements = useElements()
+   const pointer = usePointer()
+
+   function pauseTimeouts() {
+      items.pauseTimeouts()
+   }
+
+   function resumeTimeouts() {
+      items.resumeTimeouts()
+   }
+
+   /**
+    * Using a Set for better control instead of the watcher cleanup function
+    * listeners will in any case garbage collect when the notification is removed
+    */
+   const buttonSet = new Set<HTMLButtonElement>()
+
+   watch(
+      elements.items.value,
+      (newItems) => {
+         if (pointer.isHovering || pointer.isTouching) return
+
+         buttonSet.forEach((button) => {
+            if (!elements.wrapper.value?.contains(button)) buttonSet.delete(button)
+         })
+
+         newItems.forEach((el) => {
+            el.querySelectorAll('button').forEach((button) => {
+               if (buttonSet.has(button)) return
+
+               button.addEventListener('focus', pauseTimeouts)
+               button.addEventListener('blur', resumeTimeouts)
+               button.addEventListener('click', (event) => {
+                  // If keyboard
+                  if (event.detail === 0) resumeTimeouts()
+               })
+
+               buttonSet.add(button)
+            })
+         })
+      },
+      { flush: 'post' }
+   )
+}
