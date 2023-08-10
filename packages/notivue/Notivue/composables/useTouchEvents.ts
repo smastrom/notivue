a6934412d174f360d1e3f6dde01505cf62e281eb
smastrom <60471784+smastrom@users.noreply.github.com>
2023-08-10 18:04:50 +0200

    refactor useMouseEvents, useTouchEvents and useVisibilityChange to interop with NotivueSwipe and NotivueKeyboard

--- packages/notivue/Notivue/composables/useTouchEvents.ts
+++ packages/notivue/Notivue/composables/useTouchEvents.ts
@@ -1,4 +1,4 @@
-import { computed, onBeforeUnmount } from 'vue'
+import { computed, onBeforeUnmount, watchEffect } from 'vue'
 
 import { useNotivue, useItems } from '@/core/useStore'
 import { isMouse } from '@/core/utils'
@@ -6,12 +6,14 @@ import { isMouse } from '@/core/utils'
 /**
  * The logic follows this pattern:
  *
- * Every time the users touch a notification, unless
- * it's a button, all notifications will pause and automatically
- * resume after 2 seconds.
+ * Every time users touch the stream, all notifications
+ * will pause and automatically resume after 2 seconds.
  *
- * If users keep tapping on the notifications, once timeouts
+ * If users keep tapping on the stream, once timeouts
  * are resumed, they will pause again after 2 seconds and so on.
+ *
+ * This is never triggered when NotivueSwipe is used as that
+ * pointerdown event is not propagated.
  */
 
 export function useTouchEvents() {
@@ -22,16 +24,12 @@ export function useTouchEvents() {
 
    function pauseTouch(event: PointerEvent) {
       if (!isMouse(event)) {
-         const isButton = (event.target as HTMLElement).tagName === 'BUTTON'
+         items.pauseTimeouts()
+         clearTimeout(resumeTimeout)
 
-         if (!isButton) {
-            items.pauseTimeouts()
-
-            clearTimeout(resumeTimeout)
-            resumeTimeout = setTimeout(() => {
-               items.resumeTimeouts()
-            }, 2000)
-         }
+         resumeTimeout = setTimeout(() => {
+            items.resumeTimeouts()
+         }, 2000)
       }
    }
 
@@ -39,5 +37,7 @@ export function useTouchEvents() {
       clearTimeout(resumeTimeout)
    })
 
-   return computed(() => (config.pauseOnTouch.value ? { onPointerdown: pauseTouch } : {}))
+   return computed(() =>
+      config.pauseOnTouch.value && !items.isStreamFocused.value ? { onPointerdown: pauseTouch } : {}
+   )
 }
