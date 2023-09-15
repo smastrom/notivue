7f2acc97530c6a63baee075cdfd9bc0dd9fe90d8
smastrom <60471784+smastrom@users.noreply.github.com>
2023-09-15 18:05:37 +0200

    NotivueCore - Restore items length change repositioning watcher

--- packages/notivue/Notivue/composables/useRepositioning.ts
+++ packages/notivue/Notivue/composables/useRepositioning.ts
@@ -8,14 +8,22 @@ import { TransitionType as TType } from '@/core/constants'
 
 /** Set of watchers that aggressively reposition notifications. */
 export function useRepositioning() {
-   const { elements, animations, config } = useStore()
+   const { elements, animations, config, items } = useStore()
 
-   // 1. Window resize (below 1100px)
+   // 1. Items length change
+   watch(
+      () => items.getLength(),
+      (newLength) => {
+         if (newLength > 0) animations.updatePositions(TType.PUSH)
+      }
+   )
+
+   // 2. Window resize (below 1100px)
    useWindowSize(() => animations.updatePositions(TType.SILENT))
 
-   // 2. Elements 'height' change
+   // 3. Elements 'height' change
    useResizeObserver(elements.items.value, () => animations.updatePositions(TType.HEIGHT))
 
-   // 3. 'position' option change
+   // 4. 'position' option change
    watch(config.isTopAlign, () => animations.updatePositions(TType.SILENT))
 }
