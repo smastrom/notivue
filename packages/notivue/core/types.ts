51c650d064f4b7fbcbee5485724e498dd58f4097
smastrom <smastrom@proton.me>
2023-10-17 13:53:43 +0200

    Notifications - Move theme types to their folder

--- packages/notivue/core/types.ts
+++ packages/notivue/core/types.ts
@@ -168,66 +168,6 @@ export interface Push {
    destroyAll: () => void
 }
 
-// Elements
-
-export type NotivueElements = 'ol' | 'li' | 'item'
-
-// Themes
-
-export type ThemeNames = 'lightTheme' | 'pastelTheme' | 'materialTheme' | 'darkTheme' | 'slateTheme'
-
-export type NotivueTheme = Partial<Record<ThemeVars, string>>
-
-type ThemeLayoutVars =
-   | '--nv-width'
-   | '--nv-min-width'
-   | '--nv-spacing'
-   | '--nv-radius'
-   | '--nv-border-width'
-   | '--nv-icon-size'
-   | '--nv-title-size'
-   | '--nv-message-size'
-   | '--nv-shadow'
-   | '--nv-tip-width'
-   | '--nv-y-align'
-
-type ThemeGlobalColorsVars =
-   | '--nv-global-bg'
-   | '--nv-global-fg'
-   | '--nv-global-accent'
-   | '--nv-global-border'
-
-type SuccessColorsVars =
-   | '--nv-success-fg'
-   | '--nv-success-bg'
-   | '--nv-success-border'
-   | '--nv-success-accent'
-
-type ErrorColorsVars = '--nv-error-fg' | '--nv-error-bg' | '--nv-error-border' | '--nv-error-accent'
-
-type WarningColorsVars =
-   | '--nv-warning-fg'
-   | '--nv-warning-bg'
-   | '--nv-warning-border'
-   | '--nv-warning-accent'
-
-type InfoColorsVars = '--nv-info-fg' | '--nv-info-bg' | '--nv-info-border' | '--nv-info-accent'
-
-type PromiseColorsVars =
-   | '--nv-promise-fg'
-   | '--nv-promise-bg'
-   | '--nv-promise-border'
-   | '--nv-promise-accent'
-
-type ThemeVars =
-   | ThemeLayoutVars
-   | ThemeGlobalColorsVars
-   | SuccessColorsVars
-   | ErrorColorsVars
-   | WarningColorsVars
-   | InfoColorsVars
-   | PromiseColorsVars
-
 // Exported Composables
 
 export type ConfigSlice = ReturnType<typeof createConfigSlice>
