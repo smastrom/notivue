588842e7616276370f4517a1f46e979c3028b364
smastrom <smastrom@proton.me>
2024-01-02 00:41:57 +0100

    Pkg - Add astro entry point check to verify tarball script, up root deps

--- packages/notivue/scripts/verify-tarball.sh
+++ packages/notivue/scripts/verify-tarball.sh
@@ -13,7 +13,7 @@ mkdir -p $temp_dir
 
 declare -a files=(
     "README.md" "package.json" "LICENSE"
-    "dist/index.js" "dist/index.d.ts"
+    "dist/index.js" "dist/index.d.ts" "dist/astro.js"
     "dist/core/animations.css" "dist/Notifications/notifications.css"
     "nuxt/module.mjs" "nuxt/module.cjs" "nuxt/module.d.ts" "nuxt/module.json"
 )
