6cd1c5050f7d797dc317e24950f92f0508e26c28
smastrom <smastrom@proton.me>
2024-01-02 00:34:19 +0100

    Notifications - Add out-in transition on promise-resolve, reject icons

--- packages/notivue/Notifications/Notifications.vue
+++ packages/notivue/Notifications/Notifications.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, unref } from 'vue'
+import { unref, watch, shallowRef } from 'vue'
 
 import { Classes as Cx, DEFAULT_NOTIFICATIONS_PROPS } from './constants'
 
@@ -7,21 +7,29 @@ import type { NotificationsProps } from 'notivue'
 
 const props = withDefaults(defineProps<NotificationsProps>(), DEFAULT_NOTIFICATIONS_PROPS)
 
-const icon = computed(() => props.icons[props.item.type])
-const closeIcon = computed(() => props.icons.close)
+const Icon = shallowRef(props.icons[props.item.type])
+const Close = props.icons.close
+
+watch(
+   () => props.item.type,
+   (t) => (Icon.value = props.icons[t]),
+   { flush: 'sync' }
+)
 </script>
 
 <template>
    <div :class="Cx.NOTIFICATION" :data-notivue="item.type" :style="theme">
-      <template v-if="icon">
-         <Component
-            v-if="typeof icon === 'object'"
-            :is="icon"
-            :class="Cx.ICON"
-            aria-hidden="true"
-         />
-         <div v-else-if="typeof icon === 'string'" :class="Cx.ICON" aria-hidden="true">
-            {{ icon }}
+      <template v-if="Icon">
+         <Transition :name="Cx.TRANSITION" v-if="typeof Icon === 'object'" mode="out-in">
+            <Component
+               v-if="typeof Icon === 'object'"
+               :is="Icon"
+               :class="Cx.ICON"
+               aria-hidden="true"
+            />
+         </Transition>
+         <div v-else-if="typeof Icon === 'string'" :class="Cx.ICON" aria-hidden="true">
+            {{ Icon }}
          </div>
       </template>
 
@@ -31,15 +39,15 @@ const closeIcon = computed(() => props.icons.close)
       </div>
 
       <button
-         v-if="!props.hideClose && closeIcon && item.type !== 'promise'"
+         v-if="!props.hideClose && Close && item.type !== 'promise'"
          :class="Cx.CLOSE"
          :aria-label="closeAriaLabel"
          type="button"
          tabindex="-1"
          @click="item.clear"
       >
-         <Component v-if="typeof closeIcon === 'object'" :is="closeIcon" :class="Cx.CLOSE_ICON" />
-         <div v-else-if="typeof closeIcon === 'string'" aria-hidden="true" v-text="closeIcon" />
+         <Component v-if="typeof Close === 'object'" :is="Close" :class="Cx.CLOSE_ICON" />
+         <div v-else-if="typeof Close === 'string'" aria-hidden="true" v-text="Close" />
       </button>
    </div>
 </template>
