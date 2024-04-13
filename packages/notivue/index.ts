3d39727d162fc6edb06e4f51d6f520d527fc938b
smastrom <smastrom@proton.me>
2024-04-14 01:07:37 +0200

    Core - Sort imports in main entry file

--- packages/notivue/index.ts
+++ packages/notivue/index.ts
@@ -1,25 +1,24 @@
+export { push } from '@/core/createPush'
+export { push as pushAstro } from './astro/push'
+
 export { updateConfig } from '@/core/createStore'
 export { createNotivue } from '@/core/createNotivue'
 
 export { startInstance, stopInstance } from '@/core/createNotivue'
+
 export { usePush, useNotivue, useNotifications, useNotivueInstance } from '@/core/useStore'
+export { useNotivueKeyboard } from '@/NotivueKeyboard/useNotivueKeyboard'
 
 export { default as Notivue } from '@/Notivue/Notivue.vue'
 export { default as NotivueAstro } from './astro/Notivue.vue'
-
 export { default as NotivueSwipe } from '@/NotivueSwipe/NotivueSwipe.vue'
-
-export { push } from '@/core/createPush'
-export { push as pushAstro } from './astro/push'
-
-export { updateConfig } from './core/createStore'
-
 export { default as NotivueKeyboard } from '@/NotivueKeyboard/NotivueKeyboard.vue'
-export { useNotivueKeyboard } from '@/NotivueKeyboard/useNotivueKeyboard'
 
 export { default as Notifications } from '@/Notifications/Notification.vue'
 export { default as Notification } from '@/Notifications/Notification.vue'
 export { default as NotificationProgress } from '@/Notifications/NotificationProgress.vue'
+export { default as NotificationsProgress } from '@/Notifications/NotificationProgress.vue'
+
 export {
    lightTheme,
    pastelTheme,
@@ -27,13 +26,14 @@ export {
    darkTheme,
    slateTheme,
 } from '@/Notifications/themes'
+
 export { filledIcons, outlinedIcons } from '@/Notifications/icons'
 
 export { DEFAULT_CONFIG } from '@/core/constants'
 
-export * from './core/types'
-export * from './Notivue/types'
-export * from './NotivueSwipe/types'
-export * from './NotivueKeyboard/types'
-export * from './Notifications/types'
+export * from '@/core/types'
+export * from '@/Notivue/types'
+export * from '@/NotivueSwipe/types'
+export * from '@/NotivueKeyboard/types'
+export * from '@/Notifications/types'
 export * from './astro/types'
