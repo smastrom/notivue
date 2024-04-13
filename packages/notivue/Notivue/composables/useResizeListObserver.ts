d5d212d89f2aa14e5130b73de21c01e2d1f626eb
smastrom <smastrom@proton.me>
2024-04-14 00:27:53 +0200

    Notivue - Remove unnecessary ResizeObsever repositioning call

--- packages/notivue/Notivue/composables/useResizeListObserver.ts
+++ packages/notivue/Notivue/composables/useResizeListObserver.ts
@@ -3,19 +3,29 @@ import { onMounted, onBeforeUnmount, watch } from 'vue'
 export function useResizeListObserver(elements: HTMLElement[], onSizeChange: () => void) {
    let resizeObserver: ResizeObserver
 
-   const calls = new Set()
+   const calls = new WeakSet()
 
    onMounted(() => {
       resizeObserver = new ResizeObserver((entries, observer) => {
-         entries.forEach((e) => {
+         for (const e of entries) {
             if (!calls.has(e.target)) {
+               // The element is being added to the DOM, skip
                calls.add(e.target)
             } else {
-               onSizeChange()
-               calls.delete(e.target)
-               observer.unobserve(e.target)
+               // The element is being removed from the DOM and its height never changed, remove
+               if (Object.values(e.contentRect.toJSON()).every((val) => val === 0)) {
+                  calls.delete(e.target)
+                  observer.unobserve(e.target)
+               } else {
+                  // The element actually changed size, trigger callback and remove
+                  console.log('ResizeObserver Triggered')
+
+                  onSizeChange()
+                  calls.delete(e.target)
+                  observer.unobserve(e.target)
+               }
             }
-         })
+         }
       })
    })
 
