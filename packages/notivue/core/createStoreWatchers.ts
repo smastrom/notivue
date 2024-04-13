e1d9de3f8617ecc59780c7ac0e67a49d8bf1c0cf
smastrom <smastrom@proton.me>
2024-04-14 00:43:22 +0200

    Core - Rename `createWatchers`, remove repositioning watcher, get unwatchers

--- packages/notivue/core/createStoreWatchers.ts
+++ packages/notivue/core/createStoreWatchers.ts
@@ -2,24 +2,16 @@ import { watch } from 'vue'
 
 import type { NotivueStore } from 'notivue'
 
-export function createWatchers(store: NotivueStore) {
-   watch(
-      store.config.isTopAlign,
+export function createStoreWatchers(store: NotivueStore) {
+   const unwatchPosition = watch(
+      store.config.position,
       () => {
          store.animations.updatePositions({ isImmediate: true })
       },
       { flush: 'post' }
    )
 
-   watch(
-      () => store.items.length,
-      () => {
-         store.animations.updatePositions()
-      },
-      { flush: 'post' }
-   )
-
-   watch(
+   const unwatchRoot = watch(
       () => store.items.length === 0 && store.queue.length === 0,
       (isReset) => {
          if (isReset) {
@@ -30,12 +22,18 @@ export function createWatchers(store: NotivueStore) {
       { flush: 'post' }
    )
 
-   watch(
+   const unwatchAnimations = watch(
       () => store.config.animations.value.enter,
       (newEnter, prevEnter) => {
          if (newEnter !== prevEnter) {
-            store.animations.resetTransitionData()
+            store.animations.resetTransitionStyles()
          }
       }
    )
+
+   return () => {
+      unwatchPosition()
+      unwatchRoot()
+      unwatchAnimations()
+   }
 }
