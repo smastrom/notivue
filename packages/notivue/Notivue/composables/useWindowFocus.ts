b8df17350135b44bf7b75599098755ca043d6fe0
smastrom <smastrom@proton.me>
2024-04-14 11:58:52 +0200

    Notivue - Rename window focus events

--- packages/notivue/Notivue/composables/useWindowFocus.ts
+++ packages/notivue/Notivue/composables/useWindowFocus.ts
@@ -5,23 +5,23 @@ import { useStore } from '@/core/useStore'
 export function useWindowFocus() {
    const { config, timeouts } = useStore()
 
-   function onWinFocus() {
+   function onFocus() {
       if (timeouts.isStreamFocused.value) return
       if (config.pauseOnTabChange.value) timeouts.resume()
    }
 
-   function onWinBlur() {
+   function onBlur() {
       if (timeouts.isStreamFocused.value) return
       if (config.pauseOnTabChange.value) timeouts.pause()
    }
 
    onMounted(() => {
-      window.addEventListener('focus', onWinFocus)
-      window.addEventListener('blur', onWinBlur)
+      window.addEventListener('focus', onFocus)
+      window.addEventListener('blur', onBlur)
    })
 
    onBeforeUnmount(() => {
-      window.removeEventListener('focus', onWinFocus)
-      window.removeEventListener('blur', onWinBlur)
+      window.removeEventListener('focus', onFocus)
+      window.removeEventListener('blur', onBlur)
    })
 }
